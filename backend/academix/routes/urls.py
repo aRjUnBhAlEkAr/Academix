1bf382ccf70bbbfa674282a61f452dcb14642c28
aRjUnBhAlEkAr <arjunbhalekar37@gmail.com>
2025-11-03 23:30:06 +0530

    Created a dedicated directory for managing all route-specific URL configurations.

---
+++ backend/academix/routes/urls.py
@@ -0,0 +1,13 @@
+'''
+*** This File contains all the url patterns for the authentication, administrator, students, teachers and academics.
+'''
+
+from django.urls import path, include;
+# from administration import views;
+# from students import views;
+# from authentication import views;
+# from teachers import views;
+# from academics import views;
+
+urlpatterns = [
+]
