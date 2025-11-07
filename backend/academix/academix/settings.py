e0320e7c5558bd773b0d66994c61f1eeaca9b61b
aRjUnBhAlEkAr <arjunbhalekar37@gmail.com>
2025-11-07 23:23:25 +0530

    chore: load SECRET_KEY from .env instead of hardcoding in settings

--- backend/academix/academix/settings.py
+++ backend/academix/academix/settings.py
@@ -19,9 +19,9 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Initialize environ
-env = environ.Env(
-    DEBUG=(bool, False)
-)
+env = environ.Env()
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
